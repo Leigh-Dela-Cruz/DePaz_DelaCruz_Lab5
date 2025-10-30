9d2d1394f8821ca9dbfa589ced3961f9d8a11993
Leigh-Dela-Cruz <leighadrienne7@gmail.com>
2025-10-30 20:54:43 +0800

    Rotation of Speed added (Decrease and Increase)

--- DePaz_DelaCruz_Lab5/DePaz_DelaCruz_Lab5.cpp
+++ DePaz_DelaCruz_Lab5/DePaz_DelaCruz_Lab5.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <GLUT/glut.h> 
+//#include <GLUT/glut.h> <- for MacOS
+#include <GL/glut.h> //for windows
+
 using namespace std;
 
 void defaultDisplay();
 void displayTriangles();
-void displayQuad(); 
 void rotatetriangle();
 void keyboard(unsigned char key, int x, int y);
 
 
 GLfloat angle = 0.0f; // this is for rotation angle
 GLfloat scaleFactor = 1.0f; // scale factor for zoom
+GLfloat speed = 0.05f; // rotation speed
+
 
 GLfloat colors[] = {
     1.0f, 0.0f, 0.0f,
@@ -34,12 +37,7 @@ GLfloat trianglevertices[] = {
     0.0f, 0.0f, 0.0f,  -0.10f, -0.50f, 0.0f,  0.10f, -0.50f, 0.0f  // DOWN
 };
 
-GLfloat quadvertices[] = {
-    -0.5f, -0.7f, 0.0f,
-     0.5f, -0.7f, 0.0f,
-     0.5f, -0.9f, 0.0f,
-    -0.5f, -0.9f, 0.0f
-};
+
 
 
 void displayTriangles() {
@@ -60,26 +58,17 @@ void displayTriangles() {
     glPopMatrix(); // Restore the original matrix
 }
 
-void displayQuad() {
-    glColor3f(0.0f, 1.0f, 0.0f);
-    
-    glEnableClientState(GL_VERTEX_ARRAY); 
-    glVertexPointer(3, GL_FLOAT, 0, quadvertices); 
-    glDrawArrays(GL_QUADS, 0, 4); 
-    glDisableClientState(GL_VERTEX_ARRAY); 
-}
 
 
 void defaultDisplay() {
     glClear(GL_COLOR_BUFFER_BIT);
     displayTriangles();
-    displayQuad(); 
     glFlush();
 }
 
 
 void rotatetriangle() {
-    angle += 0.05f; //responsible for making the rotation slower or faster
+    angle += speed; //responsible for making the rotation slower or faster
     //currently the speed is set to slow, the smaller the value the slower the rotation
     if (angle > 360.0f) {
         angle -= 360.0f;
@@ -102,6 +91,17 @@ void keyboard(unsigned char key, int x, int y) {
             }
             cout << "Zoom Out Activated - Scale: " << scaleFactor << endl;
             break;
+        case '+':
+            speed += 0.01f;
+			cout << "Increase Speed - Speed: " << speed << endl;
+            break;
+        case '-':
+			speed -= 0.01f;
+            if (speed < 0.01f) {
+                speed = 0.01f;
+            }
+            cout << "Decrease Speed - Speed: " << speed << endl;
+            break;
     }
     glutPostRedisplay(); 
 }
@@ -112,7 +112,7 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(200, 50);
-    glutCreateWindow("Full Program with Vertex Arrays");
+    glutCreateWindow("DePaz & Dela Cruz LAB5");
     
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
     
