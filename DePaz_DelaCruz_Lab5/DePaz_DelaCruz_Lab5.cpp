1b413e7cfc51d01cebeb21ab0bf6512483e1f086
Leigh-Dela-Cruz <leighadrienne7@gmail.com>
2025-10-30 19:00:41 +0800

    Add project files.

---
+++ DePaz_DelaCruz_Lab5/DePaz_DelaCruz_Lab5.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <GL/glut.h>
+using namespace std;
+
+void defaultDisplay();
+void displayTriangles();
+
+
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f,
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f,
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f,
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+GLfloat trianglevertices[] = {
+    0.0f, 0.0f, 0.0f,  -0.5f, 0.10f, 0.0f,  -0.5f, -0.10f, 0.0f, // LEFT
+    0.0f, 0.0f, 0.0f,   0.5f, 0.10f, 0.0f,   0.5f, -0.10f, 0.0f, // RIGHT
+    0.0f, 0.0f, 0.0f,  -0.10f, 0.50f, 0.0f,   0.10f, 0.50f, 0.0f, // UP
+    0.0f, 0.0f, 0.0f,  -0.10f, -0.50f, 0.0f,  0.10f, -0.50f, 0.0f  // DOWN
+};
+
+GLfloat quadvertices[] = {
+    -0.5f, -0.7f, 0.0f,
+     0.5f, -0.7f, 0.0f,
+     0.5f, -0.9f, 0.0f,
+    -0.5f, -0.9f, 0.0f
+};
+
+void displayTriangles() {
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glColorPointer(3, GL_FLOAT, 0, colors);
+    glVertexPointer(3, GL_FLOAT, 0, trianglevertices);
+    glDrawArrays(GL_TRIANGLES, 0, 12);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+}
+
+
+void defaultDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    displayTriangles();
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(200, 50);
+    glutCreateWindow("DePaz_DelaCruz_Lab5");
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glutDisplayFunc(defaultDisplay);
+    glutMainLoop();
+    return 0;
+}
